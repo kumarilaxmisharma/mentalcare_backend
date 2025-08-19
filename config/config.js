a304bc1a880f63e6c9cd93994b88a583b4ece58b
Kumari Laxmi Sharma <kumarilaxmisharma34@gmail.com>
2025-08-19 11:55:17 +0700

    Stop tracking .env file

---
+++ config/config.js
@@ -0,0 +1,26 @@
+// config/config.js
+require('dotenv').config(); // This line reads your .env file
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+};
